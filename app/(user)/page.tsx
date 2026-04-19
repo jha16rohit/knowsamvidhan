5db43b10c7281ece25daaf11162a764c1f1db520
rohit1604jha@gmail.com <191991093+jha16rohit@users.noreply.github.com>
2026-04-19 08:31:08 +0530

    frontend folder structure done

---
+++ app/(user)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">
+        KnowSamvidhan 🚀
+      </h1>
+    </div>
+  );
+}
